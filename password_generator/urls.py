3ca140bd3e2ac2b43b193460ecca535cbdb7dc54
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-09-13 11:22:16 +0500

    passgen
    passgen

--- password_generator/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from generator import views
-
-urlpatterns = [
-    path('home', views.home, name='home'),
-    path('generatedpassword/', views.password, name='password'),
-    path('info', views.info, name='info'),
-]
