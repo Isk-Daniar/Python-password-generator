3d6235b1e168e379b4b28fdeb2e685606692ed6c
Isk-Daniar <109099588+Isk-Daniar@users.noreply.github.com>
2022-10-13 17:50:23 +0500

    fixed

--- password_generator/urls.py
+++ password_generator/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from generator import views
 
 urlpatterns = [
-    path('home', views.home, name='home'),
+    path('', views.home, name='home'),
     path('generatedpassword/', views.password, name='password'),
     path('info', views.info, name='info'),
 ]
